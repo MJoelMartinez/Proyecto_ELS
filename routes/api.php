595289d7730830dcd96c53761883a45bd08eff81
Maykol Martinez <MJoelMartinez320@gmail.com>
2023-08-11 21:09:18 -0300

    Reinicio del repositorio

--- routes/api.php
+++ routes/api.php
@@ -3,75 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UsuarioController;
-use App\Http\Controllers\AlmacenController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\ArticuloController;
-use App\Http\Controllers\PaqueteController;
-use App\Http\Controllers\LoteController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-use App\Http\Middleware\Autenticacion;
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) 
-{
-    return $request->Usuario();
-});
-
-Route::prefix('v1')->group(function ()
-{
-    Route::post('/IniciarSesion',
-        [LoginController::class, "IniciarSesion"]
-    );
-
-    Route::post('/Usuarios/Crear',
-        [UsuarioController::class, "ValidarRegistro"]
-    );
-
-    Route::put("/Backoffice/Usuarios/{documentoDeIdentidad}",
-        [UsuarioController::class, "Modificar"]
-    );
-
-    Route::delete("/Backoffice/Usuarios/{documentoDeIdentidad}",
-        [UsuarioController::class, "Eliminar"]
-    );
-
-    Route::get('/Backoffice/Usuarios/Listar',
-        [UsuarioController::class, "Listar"]
-    );
-
-    Route::post('/Backoffice/Almacenes',
-        [AlmacenController::class, "Crear"]
-    );
-
-    Route::put('/Backoffice/Almacenes/{idAlmacen}',
-        [AlmacenController::class, "Modificar"]
-    );
-
-    Route::delete('/Backoffice/Almacenes/{idAlmacen}',
-        [AlmacenController::class, "Eliminar"]
-    );
-
-    Route::post('/Backoffice/Articulos',
-        [ArticuloController::class, "CrearArticulo"]
-    );
-
-    Route::post('Almacenes/Paquetes',
-        [PaqueteController::class, "CrearPaquete"]
-    );
-
-    Route::put('/Almacenes/Paquetes/{idPaquete}',
-        [PaqueteController::class,"AsignarPeso"]
-    );
-    
-    Route::post('Almacenes/Lotes',
-        [LoteController::class, "CrearLote"]
-    );
-    
-    Route::put('/Almacenes/Articulos/{idArticulo}',
-        [ArticuloController::class, "ModificarArticulo"]
-    );
-
-    Route::delete('/Almacenes/Articulos/{idArticulo}',
-        [ArticuloController::class, "EliminarArticulo"]
-    );
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -2,42 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UsuarioController;
-use App\Http\Controllers\AlmacenController;
-use App\Http\Controllers\PaqueteController;
-use App\Http\Controllers\LoteController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/', function () 
-{
-    return view('homepage');
-});
-
-Route::get('/Backoffice', function () 
-{
-    return view('backoffice');
-});
-
-Route::get('/Backoffice/Usuarios', function () 
-{
-    return view('usuarios');
-});
-
-Route::get('/Backoffice/Almacenes', function () 
-{
-    return view('almacenes');
-});
-
-Route::get('/AppAlmacenes', function () 
-{
-    return view('appalmacenes');
-});
-
-Route::get('/AppAlmacenes/Paquetes', function () 
-{
-    return view('paquetes');
-});
-
-Route::get('/AppAlmacenes/Lotes', function () 
-{
-    return view('lotes');
+Route::get('/', function () {
+    return view('welcome');
 });
