824f506ce80d565f81f6dbe2af4b1e1d515b6f22
Belen Rosas <belenrosas97@gmail.com>
2023-10-22 23:46:12 -0300

    Agregacion del header al backoffice

--- resources/views/backoffice.blade.php
+++ resources/views/backoffice.blade.php
@@ -3,11 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../img/Icon.png">
+    <meta name="author" content="Script TriByte">
+    <link rel="stylesheet" href="/css/headerStyle.css">
+    <link rel="stylesheet" href="/css/clasesGlobales.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="icon" type="image/png" href="/img/iconoPestana.png">
     <title>E.L.S - Backoffice</title>
 </head>
 <body>
-    <label><a href="/usuarios">Usuarios del Sistema</a></label>
+    <header class="textoClaro">
+    <a href="/"><img class="logo" src="/img/Logo del Sistema.png"></a>
+    <nav>
+      <ul class="menu">
+        <li class="cambioCursor"><a href="/">HOME</a></li>
+        <li><a href="/html/opcionesHeader/acercaDe.html">ACERCA DE</a></li>
+        <li class="cambioCursor"><a href="/html/opcionesHeader/contacto.html">CONTACTO</a></li>
+        <li><div class="boton textoClaro cambioCursor divEnHeader" id="idiomaDelSistema"></div></li>
+        <li><div class="boton textoClaro cambioCursor divEnHeader" id="aparienciaDelSistema"></div></li>
+        <li>
+          <div id="contenedorUsuario">
+            <img class="usuario" id="iconoUsuario" src="/img/iconoUsuario.png">
+            <button class="boton textoClaro">NOMBRE DEL USUARIO</button>
+            <ul class="submenu">
+              <li><img class="salir" id="iconoSalida" src="/img/iconoSalir.png">
+                <a href="/html/login.html">CERRAR SESIÓN</a></li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  {{-- <label><a href="/usuarios">Usuarios del Sistema</a></label>
     <br><br>
     <label><a href="/almacenes">Almacenes de 'Quick Carry'</a></label>
     <br><br>
@@ -19,6 +45,6 @@
     <br><br>
     <label><a href="/turnos">Lista de Turnos</a></label>
     <br><br>
-    <label><a href="/tipoArticulo">Tipos de Articulo</a></label>
+    <label><a href="/tipoArticulo">Tipos de Articulo</a></label> --}}
 </body>
 </html>
