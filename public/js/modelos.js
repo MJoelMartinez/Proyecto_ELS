c6424791010d6c16701775a4a3746fb97e4ee21e
Belen Rosas <belenrosas97@gmail.com>
2023-10-30 01:03:11 -0300

    Diseño de crear modelo

--- public/js/modelos.js
+++ public/js/modelos.js
@@ -1,4 +1,23 @@
-const formularioModificarModelos = document.getElementById("formularioModificarModelos");
+$("#crearModelo").click(function() {
+    $("#ajusteBrillo").show();
+    $("#contenedorCrear").show();
+});
+
+$("#cerrarContenedorCrear").click(function() {
+    $("#ajusteBrillo").hide();
+    $("#contenedorCrear").hide();
+});
+
+$("#botonBuscar").click(function(){
+    $("#formularioBuscarModelos").attr("action", "/modelos/buscar");
+})
+
+$("#botonCrearAlmacenes").click(function(){
+    $("#formularioCrearModelos").attr("action", "/modelos/crear");
+})
+
+
+/*const formularioModificarModelos = document.getElementById("formularioModificarModelos");
 const botonFormularioModificarModelos = document.getElementById("botonFormularioModificarModelos");
 
 const formularioEliminarModelos = document.getElementById("formularioEliminarModelos");
@@ -16,4 +35,4 @@ botonFormularioEliminarModelos.addEventListener("click", function()
     let inputIDModelo = document.getElementById("inputIDModeloEliminar");
     let valorInput = inputIDModelo.value;
     formularioEliminarModelos.setAttribute("action", "api/v2/modelos/" +  valorInput);
-});
+});*/
--- resources/views/modelos.blade.php
+++ resources/views/modelos.blade.php
@@ -1,24 +1,79 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Script TriByte">
+    <link rel="stylesheet" href="/css/headerStyle.css">
+    <link rel="stylesheet" href="/css/clasesGlobales.css">
+    <link rel="stylesheet" href="/css/modelos.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="icon" type="image/png" href="/img/iconoPestana.png">
     <title>E.L.S - Lista de Modelos de Vehículos</title>
 </head>
 <body>
-    <div id="contenedorCrear">
-        <form action="api/v2/modelos" method="POST">
+<img id="ajusteBrillo" src="/img/BGNegro.png">
+    <header class="textoClaro">
+    <a href="/"><img class="logo" src="/img/Logo del Sistema.png"></a>
+    <nav>
+    <ul class="menu">
+        <li class="cambioCursor"><a href="/">HOME</a></li>
+        <li><a href="/html/opcionesHeader/acercaDe.html">ACERCA DE</a></li>
+        <li class="cambioCursor"><a href="/html/opcionesHeader/contacto.html">CONTACTO</a></li>
+        <li><div class="boton textoClaro cambioCursor divEnHeader" id="idiomaDelSistema"></div></li>
+        <li><div class="boton textoClaro cambioCursor divEnHeader" id="aparienciaDelSistema"></div></li>
+        <li>
+        <div id="contenedorUsuario">
+            <img class="usuario" id="iconoUsuario" src="/img/iconoUsuario.png">
+            <button class="boton textoClaro">NOMBRE DEL USUARIO</button>
+            <ul class="submenu">
+            <li><img class="salir" id="iconoSalida" src="/img/iconoSalir.png">
+                <a href="/html/login.html">CERRAR SESIÓN</a></li>
+            </ul>
+        </div>
+        </li>
+    </ul>
+    </nav>
+</header>
+
+<div class="contenedorModelos">
+        <div class="opcionesModelos">
+            <img src="/img/iconoAgregar.png" class="cambioCursor" id="crearModelo">
+            <label class="textoClaro"><b>|</b></label>
+            <form action="api/v1/modelos/buscar" id="formularioBuscarModelo" method="post">
+                <input class="roboto textoClaro" id="barraDeBusqueda" name="barraDeBusqueda" placeholder="Buscar Modelo">
+                <div class="roboto textoClaro" id="contenedorFiltroDeBusqueda">
+                    <label><b>Filtro de Búsqueda:</b></label>
+                    <select class="roboto textoClaro" id="filtroDeLista" name="filtroDeLista">
+                        <option value="nombre"><b>Nombre</b></option>
+                        <option value="idModelo"><b>ID Modelo</b></option>
+                        <option value="anio"><b>Año</b></option>
+                    <select>
+                </div>
+                <button type="submit" id="botonBuscar">
+                    <img class="cambioCursor" id="imagenBotonBuscar" src="/img/iconoLupa.png">
+                </button>
+            </form>
+        </div>
+        <hr>
+    </div>
+
+<div class="contenedorCrearModelos roboto textoClaro" id="contenedorCrear">
+<img class="cambioCursor" id="cerrarContenedorCrear" src="/img/iconoCerrar.png">
+        <form action="api/v2/modelos" method="POST" id="formularioCrearModelos">
             @csrf
+            <br>
             <label>Nombre: </label>
-            <input type="text" name="nombre" required>
+            <input class="inputCrearModelo textoClaro roboto" type="text" name="nombre" required>
             <br><br>
             <label>Año de creación: </label>
-            <input type="number" name="anio" min="2000" max="9999" required>
+            <input class="inputCrearModelo textoClaro roboto" type="number" name="anio" min="2000" max="9999" required>
             <br><br>
-            <button type="submit">Añadir modelo</button>
+            <button class="botonCrearModelo roboto textoClaro cambioCursor" type="submit">Añadir modelo</button>
         </form>
     </div>
-    <div id="contenedorModificar">
+
+    {{--<div id="contenedorModificar">
         <form id="formularioModificarModelos" action="api/v2/modelos" method="POST">
             @method('PUT')
             @csrf
@@ -43,7 +98,7 @@
             <br><br>
             <button id="botonFormularioEliminarModelos" type="submit">Eliminar modelo</button>
         </form>
-    </div>
+    </div>--}}
     <script src="../js/modelos.js"></script>
 </body>
 </html>
